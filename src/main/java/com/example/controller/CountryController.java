7eab64ef469f9e75f16b6532a255c1a2e0ea5242
sanjay <mappingbuddhistheritage@gmail.com>
2020-12-19 11:19:28 +0530

    update this thing in new ways

--- src/main/java/com/example/controller/CountryController.java
+++ src/main/java/com/example/controller/CountryController.java
@@ -20,13 +20,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @author Pali
  */
 @Controller
-@RequestMapping("/dashboard/view_Country")
+@RequestMapping("/dashboard")
 public class CountryController {
 
     @Autowired
     private CountryService countryService;
     
-    @GetMapping()
+    @GetMapping("/view_Country")
     public String viewCountry(Model model) {  
         model.addAttribute("listCountry", countryService.getAllCountry());
         return "view_Country";
