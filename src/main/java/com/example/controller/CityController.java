176640da85f273952ef8f765fb89a61e777e28b5
sanjay <mappingbuddhistheritage@gmail.com>
2020-12-30 12:31:11 +0530

    update this thing in new ways

--- src/main/java/com/example/controller/CityController.java
+++ src/main/java/com/example/controller/CityController.java
@@ -5,20 +5,29 @@
  */
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  *
  * @author Pali
  */
 @Controller
-@RequestMapping("/view_City")
 public class CityController {
 
-    @GetMapping()
+    @GetMapping("/dashboard/view_City")
     public String viewCity() {
         return "view_City";
+    }
+
+    @GetMapping("/dashboard/view_City/showNewCityForm")
+    public String showNewStateForm(Model model) {
+        // create model attribute to bind form data
+        return "new_City";
     }
 }
